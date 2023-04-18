695a38ce317a625b0737b495a25296b369f8f319
Trần Gia Huy <89134949+testvcps@users.noreply.github.com>
2023-04-18 23:28:48 +0700

    Add files via upload

---
+++ UnityServer/Client.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Sockets;
+
+
+namespace UnityServer
+{
+    class Client
+    {
+        public int connectionID;
+        public string ip;
+        public TcpClient socket;
+        public NetworkStream myStream;
+        private byte[] readBuffer;
+
+        public void Start()
+        {
+            socket.SendBufferSize = 4096;
+            socket.ReceiveBufferSize = 4096;
+            myStream = socket.GetStream();
+            readBuffer = new byte[4096];
+            myStream.BeginRead(readBuffer, 0, socket.ReceiveBufferSize, ReceiveDataCallback, null);
+        }
+
+        private void ReceiveDataCallback(IAsyncResult result)
+        {
+            try
+            {
+                int readbytes = myStream.EndRead(result);
+                if (readbytes <= 0)
+                {
+                    return;
+                }
+                byte[] newBytes = new byte[readbytes];
+                Buffer.BlockCopy(readBuffer, 0, newBytes, 0, readbytes);
+
+                myStream.BeginRead(readBuffer, 0, socket.ReceiveBufferSize, ReceiveDataCallback, null);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ UnityServer/TCPServer.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace UnityServer
+{
+    class TCPServer
+    {
+        private TcpListener serverSocket;
+
+        public Clients[] Client = new Clients[1500];
+
+        public void InitNetwork()
+        {
+            serverSocket = new TcpListener(IPAddress.Any, 5555);
+            serverSocket.Start();
+            serverSocket.BeginAcceptTcpClient(ClientConnectCallback, null);
+        }
+
+        private void ClientConnectCallback(IAsyncResult result)
+        {
+            TcpClient tempClient = serverSocket.EndAcceptTcpClient(result);
+            serverSocket.BeginAcceptTcpClient(ClientConnectCallback, null);
+
+            for (int i = 0; i < Client.Length; i++)
+            {
+                if (Client[i].socket == null)
+                {
+                    Client[i].socket = tempClient;
+                    Client[i].connectionID = i;
+                    Client[i].ip = tempClient.Client.RemoteEndPoint.ToString();
+                    Client[i].Start();
+                    Console.WriteLine("Connection received from " + Client[i].ip + ".");
+                    return;
+                }
+            }
+        }
+    }
+}
